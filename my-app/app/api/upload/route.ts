f6427c0c7cf9c088ad9ef701ca01d9644edfb71a
Ravindra Singh Shah <ravindrasinghshah@gmail.com>
2026-01-06 16:28:21 -0600

    route

--- my-app/app/api/upload/route.ts
+++ my-app/app/api/upload/route.ts
@@ -5,15 +5,9 @@ export async function POST(request: Request): Promise<NextResponse> {
   const { searchParams } = new URL(request.url);
   const filename = searchParams.get("filename");
 
-  // ⚠️ The below code is for App Router Route Handlers only
-  const blob = await put(filename, request.body, {
+  const blob = await put(filename ?? "", request as unknown as File, {
     access: "public",
   });
-
-  // Here's the code for Pages API Routes:
-  // const blob = await put(filename, request, {
-  //   access: 'public',
-  // });
 
   return NextResponse.json(blob);
 }
